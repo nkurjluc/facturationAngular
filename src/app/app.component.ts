7283ac1001e60d473a5e6ea7e215f303239c9640
nkurjluc <nkurjluc@gmail.com>
2025-03-13 14:06:26 -0400

    Commit après migration vers angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  //template: '<div> <h1>{{title}}</h1> <p>Bienvenue dans Facturation</p> </div>',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    //template: '<div> <h1>{{title}}</h1> <p>Bienvenue dans Facturation</p> </div>',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Facturation';
--- src/app/features/games/components/list-games/list-games.component.ts
+++ src/app/features/games/components/list-games/list-games.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list-games',
-  standalone: true,
-  imports: [],
-  templateUrl: './list-games.component.html',
-  styleUrl: './list-games.component.css'
+    selector: 'app-list-games',
+    imports: [],
+    templateUrl: './list-games.component.html',
+    styleUrl: './list-games.component.css'
 })
 export class ListGamesComponent {
 
